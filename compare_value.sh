bf8551399b2bcab7a3a9e814a8a38270e3df6083
aarondarwish <mail.aaron.darwish@gmail.com>
2023-03-22 08:25:13 +0000

    Test.

--- compare_value.sh
+++ compare_value.sh
@@ -7,6 +7,8 @@ varcomp() {
     [ "$1" = "$2" ] && return 1 || varcompe $1 $2
 }
 
+# Test the function
 varcomp 2.4.10 2.4.9 && echo "continue" || echo "don't continue" # don't continue
 varcomp 2.4.8 2.4.10 && echo "continue" || echo "don't continue" # continue
 varcomp 2.5.6 2.5.6 && echo "continue" || echo "don't continue" # don't continue
+
