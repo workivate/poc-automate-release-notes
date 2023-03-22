3b640205a87a2207161ded41d8236879e5c4d97e
aarondarwish <mail.aaron.darwish@gmail.com>
2023-03-22 09:31:10 +0000

    Test.

--- compare_value.sh
+++ compare_value.sh
@@ -12,3 +12,11 @@ varcomp 2.4.10 2.4.9 && echo "continue" || echo "don't continue" # don't continu
 varcomp 2.4.8 2.4.10 && echo "continue" || echo "don't continue" # continue
 varcomp 2.5.6 2.5.6 && echo "continue" || echo "don't continue" # don't continue
 
+
+if [ "$(printf '%s\n' "1.1.1" "1.1.0" | sort -V | head -n1)" = "1.1.1" ]; then
+    echo "ERROR"
+    exit 1
+else
+    echo "OK"
+    exit 0
+fi
